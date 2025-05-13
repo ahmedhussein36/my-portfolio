e1f83d55e93f7d302f2359b3fa5abb53eaf65683
Ahmed Hussein <105352918+ahmedhussein36@users.noreply.github.com>
2025-05-13 14:50:24 +0300

    commit

--- app/components/DownloadCv.tsx
+++ app/components/DownloadCv.tsx
@@ -1,22 +1,16 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 const DownloadCVButton = () => {
-    const handleDownload = () => {
-        // Define the file URL (relative to public folder or absolute URL)
-        const fileUrl = "/Ahmed-Hussein-frontend-developer-resume-2025.pdf"; // Adjust path if needed
-        const link = document.createElement("a");
-        link.href = fileUrl;
-        link.download = "Ahmed-Hussein-frontend-developer-resume-2025.pdf"; // Optional: specify downloaded file name
-        link.click();
-    };
-
     return (
-        <Button
-            className="mt-4 bg-slate-50 text-black hover:bg-slate-200"
-            onClick={handleDownload}
-        >
-            Download My Resume
+        <Button className="mt-4 bg-slate-50 text-black hover:bg-slate-200">
+            <Link
+                href={"/Ahmed-Hussein-frontend-developer-resume-2025.pdf"}
+                target="_blank"
+            >
+                My Resume
+            </Link>
         </Button>
     );
 };
